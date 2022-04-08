486060b343be62dd933410e5e7d3de194d6a9590
liuwei9 <liuwei199805@yeah.net>
2022-04-08 21:04:19 +0800

    update shape

---
+++ src/main/scala/shape/Shape.scala
@@ -0,0 +1,41 @@
+package shape
+
+import spinal.core._
+import spinal.lib._
+
+case class ShapeConfig(DATA_WIDTH: Int, COMPUTE_CHANNEL_NUM: Int, FEATURE: Int, CHANNEL_WIDTH: Int, MAX_ROW_MEM_DEPTH: Int, UP_ROW_MEM_DEPTH: Int) {
+    val STREAM_DATA_WIDTH = DATA_WIDTH * COMPUTE_CHANNEL_NUM
+
+    val concatConfig = ConcatConfig(DATA_WIDTH, COMPUTE_CHANNEL_NUM, FEATURE, CHANNEL_WIDTH)
+    val splitConfig = SplitConfig(DATA_WIDTH, COMPUTE_CHANNEL_NUM, FEATURE, CHANNEL_WIDTH)
+    val upSamplingConfig = UpSamplingConfig(DATA_WIDTH, COMPUTE_CHANNEL_NUM, FEATURE, CHANNEL_WIDTH, UP_ROW_MEM_DEPTH)
+    val maxPoolingConfig = MaxPoolingConfig(DATA_WIDTH, COMPUTE_CHANNEL_NUM, FEATURE, CHANNEL_WIDTH, MAX_ROW_MEM_DEPTH)
+
+}
+
+
+class Shape(shapeConfig: ShapeConfig) extends Component {
+    val io = new Bundle {
+        val sData = Vec(slave Stream (UInt(shapeConfig.STREAM_DATA_WIDTH bits)), 2)
+        val mData = master Stream (UInt(shapeConfig.STREAM_DATA_WIDTH bits))
+        val dmaReadValid = out Vec(Bool(), 2)
+        val dmaWriteValid = out Bool()
+        val control = in Bits (4 bits)
+        val state = out Bits (4 bits)
+        val introut = in Bool()
+    }
+    noIoPrefix()
+
+    val shapeState = new ShapeState
+    shapeState.io.dmaReadValid <> io.dmaReadValid
+    shapeState.io.dmaWriteValid <> io.dmaWriteValid
+    shapeState.io.control <> io.control
+    shapeState.io.state <> io.state
+
+    val concat = new Concat(shapeConfig.concatConfig)
+
+}
+
+object Shape extends App {
+    SpinalVerilog(new Shape(ShapeConfig(8, 8, 640, 10, 1024, 1024)))
+}
