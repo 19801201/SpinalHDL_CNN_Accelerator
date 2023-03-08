85563360ebe0ee85580a303043fd96615e1997f9
13230668653 <cqyinsist@qq.com>
2023-03-08 15:03:14 +0800

    添加计数器初始值，调整计数器初值可以记录更多的数。

--- src/main/scala/wa/WaCounter.scala
+++ src/main/scala/wa/WaCounter.scala
@@ -3,11 +3,12 @@ package wa
 import spinal.core.{Area, Bool, False, IntToBuilder, Reg, True, UInt, when}
 
 object WaCounter {
-    def apply(en: Bool, width: Int, end: UInt) = new WaCounter(en, width, end)
+    def apply(en: Bool, width: Int, end: UInt) = new WaCounter(en, width, end, 0)
+    def apply(en: Bool, width: Int, end: UInt, InitData: Int) = new WaCounter(en, width, end, InitData)
 }
 
-class WaCounter(en: Bool, width: Int, cnt: UInt) extends Area {
-    val count = Reg(UInt(width bits)) init 0
+class WaCounter(en: Bool, width: Int, cnt: UInt, InitData: Int) extends Area {
+    val count = Reg(UInt(width bits)) init InitData
     val valid = Bool()
     when(count === cnt) {
         valid := True
@@ -22,7 +23,7 @@ class WaCounter(en: Bool, width: Int, cnt: UInt) extends Area {
     }
 
     def clear = {
-        count := 0
+        count := InitData
         valid := False
     }
 }
